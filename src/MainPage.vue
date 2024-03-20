fdbb0c2b8b10b8bbc0404654477540e2a809b660
Roland Brilianto <rolandbrilianto@gmail.com>
2024-03-20 15:52:06 +0700

    fix(navbar): doing section bug

--- src/MainPage.vue
+++ src/MainPage.vue
@@ -1,7 +1,16 @@
 <template>
   <header-nav></header-nav>
+
+  <about-me></about-me>
+  <education></education>
+  <experiences></experiences>
+  <projects></projects>
 </template>
 
 <script setup>
 import HeaderNav from "./components/HeaderNav.vue";
+import AboutMe from "./components/AboutMe.vue";
+import Projects from "./components/Projects.vue";
+import Education from "./components/Education.vue";
+import Experiences from "./components/Experiences.vue";
 </script>
--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,21 +1,26 @@
 <template>
-  <header class="bg-blue-500 min-h-16">
-    <section class="py-3 border-b border-gray-600">
-      rolandbrilianto@gmail.com
+  <header class="bg-white shadow-md min-h-16 fixed w-full z-0 top-0">
+    <section class="py-3 border-b border-gray-600 flex space-x-4">
+      <FormKitIcon icon="email" class="w-6 my-auto ml-7 text-main-blue" />
+      <span class="text-gray-700">rolandbrilianto@gmail.com</span>
     </section>
     <div class="container mx-auto flex items-center justify-between">
       <!-- Logo di kiri -->
-      <div class="flex space-x-2">
+      <div class="flex">
         <img src="@/assets/newIcon.png" alt="Logo" class="w-12" />
         <span class="font-semibold text-lg my-auto">Roland Brilianto</span>
       </div>
       <!-- Section di kanan -->
       <nav>
-        <ul class="flex space-x-16">
-          <li><a href="#" class="hover:text-gray-300">About Me</a></li>
-          <li><a href="#" class="hover:text-gray-300">Education</a></li>
-          <li><a href="#" class="hover:text-gray-300">Experiences</a></li>
-          <li><a href="#" class="hover:text-gray-300">Projects</a></li>
+        <ul class="flex space-x-16 text-gray-700 uppercase">
+          <li><a href="#about" class="hover:text-gray-300">About Me</a></li>
+          <li>
+            <a href="#education" class="hover:text-gray-300">Education</a>
+          </li>
+          <li>
+            <a href="#experiences" class="hover:text-gray-300">Experiences</a>
+          </li>
+          <li><a href="#project" class="hover:text-gray-300">Projects</a></li>
           <li><a href="#" class="hover:text-gray-300">Skills</a></li>
           <li><a href="#" class="hover:text-gray-300">Contact Me</a></li>
         </ul>
@@ -23,3 +28,7 @@
     </div>
   </header>
 </template>
+
+<script setup>
+import { FormKitIcon } from "@formkit/vue";
+</script>
